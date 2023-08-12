417dd43fb3f303558cef673238a6c4f35d551b68
satish-tanavade <satishtanavade6363@gmail.com>
2023-08-12 13:20:48 +0530

    image change

--- src/task5/pages/DashBoard.js
+++ src/task5/pages/DashBoard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function DashBoard() {
   return (
     <>
-      <img className=' img-fluid img-thumbnail' width={'100%'} height={'100%'} backgroundColor = {'black'}  src='https://www.unishivaji.ac.in/uploads/home_photos/Shivaji-University-Kolhapur.jpg' alt='Shivaji University'/>
+      <img className=' img-fluid img-thumbnail' style={{ height: '100vh' , objectFit:'cover', position:'fixed'}}  src='https://www.unishivaji.ac.in/uploads/home_photos/Shivaji-University-Kolhapur.jpg' alt='Shivaji University'/>
     </>
   )
 }
